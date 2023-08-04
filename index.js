233405b470d14b850c353f82fa614cf82f577142
Ripika Sharma <ripikasharma@gmail.com>
2023-08-04 17:19:16 -0400

    dom

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement= document.getElementById("main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent="Ripika is the champion ";
